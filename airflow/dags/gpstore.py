e9d9a52a72f9022a977a1954dd4569c1bc853186
Drimkore <art.2000m.ab@gmail.com>
2023-12-04 23:38:07 +0500

    fix

--- airflow/dags/gpstore.py
+++ airflow/dags/gpstore.py
@@ -24,25 +24,25 @@ with DAG(
     extract_data = BashOperator(task_id='extract_data',
                             bash_command="mkdir data && cd data && mkdir raw && unzip -o -q /home/art/gitprj/MLopsXFlow/datasets/archive.zip -d /home/art/gitprj/MLopsXFlow/datasets/data/raw/", 
                             dag=dag)
-    data_one = BashOperator(task_id='data_one',
-                            bash_command="python /home/art/gitprj/MLopsXFlow/scripts/data_onep.py", 
+    data_prep_one = BashOperator(task_id='data_prep_one',
+                            bash_command="python /home/art/gitprj/MLopsXFlow/scripts/data_prep_one.py", 
                             dag=dag)
-    data_second = BashOperator(task_id='data_second',
-                            bash_command="python /home/art/gitprj/MLopsXFlow/scripts/data_second.py", 
+    data_prep_second = BashOperator(task_id='data_prep_second',
+                            bash_command="python /home/art/gitprj/MLopsXFlow/scripts/data_prep_second.py", 
                             dag=dag)
-    data_third = BashOperator(task_id='data_third',
-                            bash_command="python /home/art/gitprj/MLopsXFlow/scripts/data_third.py", 
+    data_prep_third = BashOperator(task_id='data_prep_third',
+                            bash_command="python /home/art/gitprj/MLopsXFlow/scripts/data_prep_third.py", 
                             dag=dag)
-    data_fourth = BashOperator(task_id='data_fourth',
-                            bash_command="cd /home/art/gitprj/MLopsXFlow/ && python /home/art/gitprj/MLopsXFlow/scripts/data_fourth.py", 
+    data_split_fourth = BashOperator(task_id='data_split_fourth',
+                            bash_command="cd /home/art/gitprj/MLopsXFlow/ && python /home/art/gitprj/MLopsXFlow/scripts/data_split_fourth.py", 
                             dag=dag)
-    data_fifth = BashOperator(task_id='data_fifth',
-                            bash_command="cd /home/art/gitprj/MLopsXFlow/ && python /home/art/gitprj/MLopsXFlow/scripts/data_fifth.py", 
+    data_train_fifth = BashOperator(task_id='data_train_fifth',
+                            bash_command="cd /home/art/gitprj/MLopsXFlow/ && python /home/art/gitprj/MLopsXFlow/scripts/data_train_fifth.py", 
                             dag=dag)
-    data_six = BashOperator(task_id='data_six',
-                            bash_command="cd /home/art/gitprj/MLopsXFlow/ && python /home/art/gitprj/MLopsXFlow/scripts/data_six.py", 
+    data_test_six = BashOperator(task_id='data_test_six',
+                            bash_command="cd /home/art/gitprj/MLopsXFlow/ && python /home/art/gitprj/MLopsXFlow/scripts/data_test_six.py", 
                             dag=dag)
 
-    get_data >> extract_data >> data_one >> data_second >> data_third >> data_fourth >> data_fifth >> data_six
+    get_data >> extract_data >> data_prep_one >> data_prep_second >> data_prep_third >> data_split_fourth >> data_train_fifth >> data_test_six
 
 
