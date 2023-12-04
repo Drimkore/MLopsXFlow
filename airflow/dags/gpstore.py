07d6e3efadc31779836c29176d471134e523240b
Drimkore <art.2000m.ab@gmail.com>
2023-12-04 23:09:24 +0500

    fix

--- airflow/dags/gpstore.py
+++ airflow/dags/gpstore.py
@@ -19,10 +19,10 @@ with DAG(
     tags=['google', 'score'],
 ) as dag:
     get_data = BashOperator(task_id='get_data',
-                            bash_command="cd /home/art/gitprj/MLopsXFlow/datasets | gdown 1i1W2wtpQErLjJdbVVThsqFd_oE2BDspj", 
+                            bash_command="cd /home/art/gitprj/MLopsXFlow/datasets && gdown 1i1W2wtpQErLjJdbVVThsqFd_oE2BDspj", 
                             dag=dag)
     extract_data = BashOperator(task_id='extract_data',
-                            bash_command="unzip -o -q archive.zip -d ./data/raw/", 
+                            bash_command="mkdir data && cd data && mkdir raw && unzip -o -q /home/art/gitprj/MLopsXFlow/datasets/archive.zip -d /home/art/gitprj/MLopsXFlow/datasets/data/raw/", 
                             dag=dag)
     data_one = BashOperator(task_id='data_one',
                             bash_command="python /home/art/gitprj/MLopsXFlow/scripts/data_onep.py", 
@@ -34,13 +34,13 @@ with DAG(
                             bash_command="python /home/art/gitprj/MLopsXFlow/scripts/data_third.py", 
                             dag=dag)
     data_fourth = BashOperator(task_id='data_fourth',
-                            bash_command="python /home/art/gitprj/MLopsXFlow/scripts/data_fourth.py", 
+                            bash_command="cd /home/art/gitprj/MLopsXFlow/ && python /home/art/gitprj/MLopsXFlow/scripts/data_fourth.py", 
                             dag=dag)
     data_fifth = BashOperator(task_id='data_fifth',
-                            bash_command="python /home/art/gitprj/MLopsXFlow/scripts/data_fifth.py", 
+                            bash_command="cd /home/art/gitprj/MLopsXFlow/ && python /home/art/gitprj/MLopsXFlow/scripts/data_fifth.py", 
                             dag=dag)
     data_six = BashOperator(task_id='data_six',
-                            bash_command="python /home/art/gitprj/MLopsXFlow/scripts/data_six.py", 
+                            bash_command="cd /home/art/gitprj/MLopsXFlow/ && python /home/art/gitprj/MLopsXFlow/scripts/data_six.py", 
                             dag=dag)
 
     get_data >> extract_data >> data_one >> data_second >> data_third >> data_fourth >> data_fifth >> data_six
